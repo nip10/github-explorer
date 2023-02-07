415519173696cd4eb70ea9929a083700c67ba191
Diogo Cardoso <diogocardoso92@outlook.com>
2023-02-07 14:28:19 +0000

    fix auth hooks

--- lib/supabase/useLogin.ts
+++ lib/supabase/useLogin.ts
@@ -1,22 +1,18 @@
 import { useCallback } from "react";
-import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import type { Database } from "@/db/types";
 
 const useLogin = () => {
   const supabaseClient = useSupabaseClient<Database>();
-  const user = useUser();
   const login = useCallback(
     async ({ email, password }: { email: string; password: string }) => {
-      if (!user) {
-        return;
-      }
       const { error } = await supabaseClient.auth.signInWithPassword({
         email,
         password,
       });
       return { error };
     },
-    [supabaseClient, user]
+    [supabaseClient]
   );
   return login;
 };
--- lib/supabase/useSignup.ts
+++ lib/supabase/useSignup.ts
@@ -1,22 +1,18 @@
 import { useCallback } from "react";
-import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import type { Database } from "@/db/types";
 
 const useSignup = () => {
   const supabaseClient = useSupabaseClient<Database>();
-  const user = useUser();
   const signup = useCallback(
     async ({ email, password }: { email: string; password: string }) => {
-      if (!user) {
-        return;
-      }
       const { error } = await supabaseClient.auth.signUp({
         email,
         password,
       });
       return { error };
     },
-    [supabaseClient, user]
+    [supabaseClient]
   );
   return signup;
 };
